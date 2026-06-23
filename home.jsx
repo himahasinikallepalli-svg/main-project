4c5b23b4696a6ed24b34d6f2b99fa818f37dd24e
hasini team <himahasini.kallepalli@gmail.com>
2026-06-23 14:39:13 +0530

    Initial commit

---
+++ home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className="home">
+      <h1>Welcome to Job Portal</h1>
+      <p>Find your dream job here.</p>
+
+      <button>Browse Jobs</button>
+    </div>
+  );
+}
+
+export default Home;
